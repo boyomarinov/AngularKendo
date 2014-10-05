6dbd549ce43d8c59eb9e3c2b9e0890a959667131
Boyan Marinov <boyan.marinov@yahoo.com>
2014-10-05 19:27:33 +0300

    Experiment with passing options to a directive * added grid directive * passed k-options for kendo-grid

--- AngularKendoTest/scripts/controllers/mainController.js
+++ AngularKendoTest/scripts/controllers/mainController.js
@@ -6,4 +6,21 @@
     $scope.color1 = "";
     $scope.test = 'bugfix6 test';
     $scope.master = 'master test';
-});
+    $scope.gOptions = {
+        dataSource: [
+            { FirstName: 'f1', LastName: 'l1' },
+            { FirstName: 'f2', LastName: 'l2' }
+        ],
+        sortable: true,
+        pageable: true,
+        columns: [{
+            field: "FirstName",
+            title: "First Name",
+            width: "120px"
+        }, {
+            field: "LastName",
+            title: "Last Name",
+            width: "120px"
+        }]
+    };
+}); 
---
+++ AngularKendoTest/scripts/directives/grid-directive/grid-directive.js
@@ -0,0 +1,35 @@
+﻿function GridDirective() {
+    return {
+        restrict: 'AEC',
+        scope: {
+            gridOptions: '=options'
+        },
+        templateUrl: 'scripts/directives/grid-directive/grid-directive.tpl.html',
+        link: GridDirectiveLink
+    }
+}
+
+function GridDirectiveLink(scope, element, attrs) {
+    console.log(attrs);
+
+    //scope.gridOptions = {
+    //    dataSource: [
+    //        { FirstName: 'f1', LastName: 'l1' },
+    //        { FirstName: 'f2', LastName: 'l2' },
+    //        { FirstName: 'f3', LastName: 'l3' },
+    //    ],
+    //    sortable: true,
+    //    pageable: true,
+    //    columns: [{
+    //            field: "FirstName",
+    //            title: "First Name",
+    //            width: "120px"
+    //        }, {
+    //            field: "LastName",
+    //            title: "Last Name",
+    //            width: "120px"
+    //        }]
+    //};
+}
+
+app.directive('myGrid', GridDirective);
